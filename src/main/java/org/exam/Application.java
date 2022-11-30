102b5454f1ac880d568583d7ae86902997b57de0
kkong101 <aflhzv@gmail.com>
2022-11-30 22:54:28 +0900

    [Feature] 아시아/한국 시간 설정 적용

--- src/main/java/org/exam/Application.java
+++ src/main/java/org/exam/Application.java
@@ -4,9 +4,18 @@ package org.exam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
 }
